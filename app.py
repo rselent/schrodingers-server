3bf0f35eead6fb10792280f298a185fa51ac58e3
rselent <rselent@ufl.edu>
2021-01-06 23:29:53 -0500

    create app.py, added header

---
+++ app.py
@@ -0,0 +1,13 @@
+# Schrödinger's Server
+# by Raymond Selent
+#
+# Purpose: register and ping URL, and display UP/DOWN status
+#		   (this is expected to change over time)
+#
+# Requirements: Python (3.9)
+#
+#
+#
+# ==========================================================
+# ==========================================================
+
